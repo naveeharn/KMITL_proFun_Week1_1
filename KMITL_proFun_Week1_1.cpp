266ad90ee7c7aae418b691a72279a08c8bdb7b49
Naveeharn Tehmarn <naveeharntehmarn@MacBook-Pro-khxng-Naveeharn.local>
2020-08-12 14:02:55 +0700

    first commit

---
+++ KMITL_proFun_Week1_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    char name[30];
+    cin>>name;
+    cout<<"Hello "<<name<<"\nWelcome to Visual Studio Code!";
+    cout<<endl;
+
+    return 0;
+}
